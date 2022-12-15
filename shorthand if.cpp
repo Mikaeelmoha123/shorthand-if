da652993936d7f5101b4f823979649e623eb2b98
Mikaeel <95021467+Mikaeelmoha123@users.noreply.github.com>
2022-12-14 22:47:00 -0800

    Add files via upload

---
+++ shorthand if.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double mark;
+    cout << "Please input mark:";
+    cin >> mark;
+    if (mark >= 50)
+        cout << "Passed\n";
+    else
+        cout << "Failed!\n";
+    system("pause");
+    return 0;
+}
